3bb4deade9be68147aa21a972c50c9685b432d50
Sid200026 <siddharthsingharoy@gmail.com>
2019-10-25 00:53:08 +0530

    Added models

--- UrlShortener/urlgen/models.py
+++ UrlShortener/urlgen/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class url(models.Model):
+    big_url = models.URLField(max_length=200)
+    short_url = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.short_url
+    
