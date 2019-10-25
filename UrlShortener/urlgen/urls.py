d8df42dc52c007019ea3c1aff0e16176989839c1
Sid200026 <siddharthsingharoy@gmail.com>
2019-10-25 16:52:59 +0530

    Finished

--- UrlShortener/urlgen/urls.py
+++ UrlShortener/urlgen/urls.py
@@ -5,6 +5,7 @@ app_name = 'urlgen'
 
 urlpatterns = [
     path('', views.home, name = "home"),
-    path('error', views.error, name = "error"),     
-    path('<slug:detail>', views.redirect, name = "redirect"),   
+    path('error', views.error, name = "error"),   
+    path('<slug:detail>', views.bigredirect, name = "bigredirect"),  
+    path('redirect/<slug:detail>', views.redirect, name = "redirect"),   
 ]
--- UrlShortener/urlgen/views.py
+++ UrlShortener/urlgen/views.py
@@ -8,31 +8,53 @@ from .models import url
 # Create your views here.
 def home(request):
     if request.method == 'POST':
-        bigurl = request.POST.get('bigurl')
-        shorturl = request.POST.get('shorturl') # None if empty
-        if shorturl == None:
-            if url.objects.filter(big_url = bigurl).exists():
-                urlobj = url.objects.filter(big_url = bigurl)[0]
-                return HttpResponseRedirect('{0}'.format(urlobj.short_url))
-        else:
-            if not url.objects.filter(short_url = shortcut).exists():
-                url.objects.create(big_url = bigurl, short_url = shortcut)
-                return HttpResponseRedirect('{0}'.format(shortcut))
-        notunique = True
-        while(notunique):
-            res = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 5)) 
-            if url.objects.filter(short_url = res).exists():
-                pass
+        urltest = None
+        try:
+            bigurl = request.POST.get('bigurl')
+            if bigurl[0:5]!='https':
+                bigurl = 'https://'+bigurl
+            shortcut = request.POST.get('preferred') # None if empty
+            if not shortcut:
+                if url.objects.filter(big_url = bigurl).exists():
+                    urlobj = url.objects.filter(big_url = bigurl)[0]
+                    return HttpResponseRedirect('redirect/{0}'.format(urlobj.short_url))
             else:
-                shortcut = res
-                break
-        url.objects.create(big_url = bigurl, short_url = shortcut)
-        return HttpResponseRedirect('{0}'.format(shortcut))
+                if not url.objects.filter(short_url = shortcut).exists():
+                    url.objects.create(big_url = bigurl, short_url = shortcut)
+                    urltest = url.objects.get(short_url = shortcut)
+                    urltest.full_clean()
+                    return HttpResponseRedirect('redirect/{0}'.format(shortcut))
+                else:
+                    print(shortcut)
+                    return render(request, 'urlgen/home.html', {'error':'The path has already been taken'})
+            notunique = True
+            while(notunique):
+                res = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 5)) 
+                if url.objects.filter(short_url = res).exists():
+                    pass
+                else:
+                    shortcut = res
+                    break
+            url.objects.create(big_url = bigurl, short_url = shortcut)
+            urltest = url.objects.get(short_url = shortcut)
+            urltest.full_clean()
+            return HttpResponseRedirect('redirect/{0}'.format(shortcut))
+        except:
+            urltest.delete()
+            return render(request, 'urlgen/home.html', {'error':'Not an URL'})
     return render(request, 'urlgen/home.html')
 
 def redirect(request, detail):
-    return render(request, 'urlgen/redirect.html')
+    shorturl = detail
+    urlobj = url.objects.get(short_url = shorturl)
+    return render(request, 'urlgen/redirect.html', {'url':urlobj})
 
 def error(request):
     return render(request, 'urlgen/error.html')
 
+def bigredirect(request, detail):
+    if not url.objects.filter(short_url = detail).exists():
+        return HttpResponseRedirect(reverse('urlgen:error'))
+    else:
+        urlobj = url.objects.get(short_url = detail)
+        return HttpResponseRedirect("{0}".format(urlobj.big_url))
